b06bd067e404d447df72f2a0de1cde3ea9a20477
Muadz Yamani <muadzyamani@gmail.com>
2025-08-19 22:45:08 +0800

    Change greeting messages to suit to chatbot

--- src/main/java/Anis.java
+++ src/main/java/Anis.java
@@ -1,10 +1,24 @@
 public class Anis {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = """
+               ░███               ░██           
+              ░██░██                            
+             ░██  ░██  ░████████  ░██ ░███████  
+            ░█████████ ░██    ░██ ░██░██        
+            ░██    ░██ ░██    ░██ ░██ ░███████  
+            ░██    ░██ ░██    ░██ ░██       ░██ 
+            ░██    ░██ ░██    ░██ ░██ ░███████                                    
+            """;
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hi! My name is Anis, and I'm here to help.");
+        System.out.println(" What's on your mind?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Glad I could assist! Have a wonderful day.");
+        System.out.println(" Feel free to reach out anytime.");
+        System.out.println("____________________________________________________________");
+
+
+
     }
 }
