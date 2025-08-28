6f9e316040400d73cf351223e47af23b2a2acd7a
Muadz Yamani <muadzyamani@gmail.com>
2025-08-28 15:46:11 +0800

    Add task marking and unmarking functionality

--- src/main/java/Anis.java
+++ src/main/java/Anis.java
@@ -13,7 +13,7 @@ public class Anis {
                  ░██    ░██ ░██    ░██ ░██ ░███████
             """;
 
-    private static final String[] tasks = new String[100];
+    private static final Task[] tasks = new Task[100];
     private static int taskCount = 0;
 
     static void printBorder() {
@@ -38,7 +38,7 @@ public class Anis {
         printBorder();
     }
 
-    public static void addTask(String task) {
+    public static void addTask(String description) {
         if (taskCount >= tasks.length) {
             printBorder();
             System.out.println("\t Task list is full. Cannot add more tasks.");
@@ -46,9 +46,11 @@ public class Anis {
             return;
         }
 
-        tasks[taskCount] = task;
+        tasks[taskCount] = new Task(description);
         taskCount++;
-        System.out.println("\t added: " + task);
+
+        printBorder();
+        System.out.println("\t added: " + description);
         printBorder();
     }
 
@@ -57,6 +59,40 @@ public class Anis {
         for (int i = 0; i < taskCount; i++) {
             System.out.println("\t " + (i + 1) + ". " + tasks[i]);
         }
+        printBorder();
+    }
+
+    public static void markTask(int taskNumber) {
+        if (taskNumber <= 0 || taskNumber > taskCount) {
+            printBorder();
+            System.out.println("\t Invalid task number.");
+            printBorder();
+            return;
+        }
+
+        int taskIndex = taskNumber - 1;
+        tasks[taskIndex].markAsDone();
+
+        printBorder();
+        System.out.println("\t Nice! I've marked this task as done:");
+        System.out.println("\t   " + tasks[taskIndex]);
+        printBorder();
+    }
+
+    public static void unmarkTask(int taskNumber) {
+        if (taskNumber <= 0 || taskNumber > taskCount) {
+            printBorder();
+            System.out.println("\t Invalid task number.");
+            printBorder();
+            return;
+        }
+
+        int taskIndex = taskNumber - 1;
+        tasks[taskIndex].unmarkAsDone();
+
+        printBorder();
+        System.out.println("\t OK, I've marked this task as not done yet:");
+        System.out.println("\t   " + tasks[taskIndex]);
         printBorder();
     }
 
@@ -79,6 +115,29 @@ public class Anis {
             case "list":
                 listTasks();
                 break;
+            case "mark":
+            case "unmark":
+                try {
+                    if (words.length < 2) {
+                        printBorder();
+                        System.out.println("\t Please specify which task to " + command + ".");
+                        printBorder();
+                        break;
+                    }
+
+                    int taskNumber = Integer.parseInt(words[1]);
+                    if (command.equals("mark")) {
+                        markTask(taskNumber);
+                    } else {
+                        unmarkTask(taskNumber);
+                    }
+                } catch (NumberFormatException e) {
+                    printBorder();
+                    System.out.println("\t Please enter a valid task number after 'mark' or 'unmark'.");
+                    printBorder();
+                }
+                break;
+
             default:
                 addTask(userInput);
                 break;
