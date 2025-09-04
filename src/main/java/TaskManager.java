5eb09df4f4acd7fb7cb17b1e5fffbfd9e775227b
Muadz Yamani <muadzyamani@gmail.com>
2025-09-04 21:13:24 +0800

    Add TaskManager class for task management functionality

---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private final List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public boolean isInvalidTaskNumber(int taskNumber) {
+        return taskNumber <= 0 || taskNumber > tasks.size();
+    }
+
+    public Task getTask(int taskNumber) {
+        return tasks.get(taskNumber - 1);
+    }
+
+    public int getTaskCount() {
+        return tasks.size();
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+}
